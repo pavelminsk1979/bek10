577ee784e65beba58639acd79c9c2e17f5da8a68
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-04-08 23:41:10 +0300

    mongoose...

--- src/db/mongoDb.ts
+++ src/db/mongoDb.ts
@@ -1,4 +1,3 @@
-import {MongoClient} from 'mongodb'
 import dotenv from 'dotenv'
 import {Blog} from "../allTypes/blogTypes";
 import {Post} from "../allTypes/postTypes";
@@ -14,37 +13,8 @@ dotenv.config()
 
 const mongoUri = process.env.MONGO_URL ;
 
-/*MONGO_URL -- представляет URL для подключения к MongoDB, использую MongoDB Atlas
-*/
 
 
-/*if(!mongoUri){
-    throw new Error('URL not find(file mongoDb/1')
-}*/
-
-
-//const client = new MongoClient(mongoUri)
-
-
-//const db = client.db('projectHW')
-
-
-
-
-/*export const postsCollection = db.collection<Post>('posts')
-
-export const blogsCollection = db.collection<Blog>('blogs')
-
-export const usersCollection = db.collection<User>('users')
-
-export const commentsCollection = db.collection<Comment>('comments')
-
-export const visitsCollection = db.collection<Visit>('visits')
-
-export const usersDevicesCollection = db.collection<UsersDevices>('usersDevices')*/
-
-
-//снизу Scheme  для  mongoose
 const userScheme = new mongoose.Schema<User>({
     passwordHash: String,
     login: String,
@@ -59,12 +29,10 @@ const userScheme = new mongoose.Schema<User>({
 })
 
 
-//снизу userOutputModel  для  mongoose
-
 export const usersModel = mongoose.model<User>('User', userScheme, 'users');
-    //'users'   в базе данных будет подраздел под таким названием
 
-////////////////////////////////////////////////////////////////
+
+
 
 const postScheme = new mongoose.Schema<Post>({
     title: String,
@@ -125,22 +93,16 @@ export const usersDevicesModel = mongoose.model<UsersDevices>('UsersDevices', us
 
 export async function runDb() {
     try {
-        //await client.connect()
-
-        //снизу  для  mongoose
         if(!mongoUri){
-            throw new Error('URL not find(file mongoDb/2')
+            throw new Error('URL not find(file mongoDb')
         }
         await mongoose.connect(mongoUri ,{ dbName:process.env.DB_NAME });
 
-        /*  ---MONGO_URL -- представляет URL для подключения к MongoDB, используя MongoDB Atlas
-          ---dbName- устанавливаю  имя базе данных*/
-
         console.log('Connected successful with mongoDB')
+
     } catch (e) {
         console.log(e + 'Connected ERROR with mongoDB')
-        //await client.close()
-        //снизу  для mongoose
+
         await mongoose.disconnect()
     }
 }
